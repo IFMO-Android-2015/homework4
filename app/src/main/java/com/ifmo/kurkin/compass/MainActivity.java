c8e9b451b306ad619bbc63b0aa1a58818a3b527a
Dmitry Kurkin <kurkindmit@gmail.com>
2015-11-16 00:17:55 +0300

    fix

---
+++ app/src/main/java/com/ifmo/kurkin/compass/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ifmo.kurkin.compass;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+  private Sensor mSensor;
+    private CompassView compassView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+        compassView = (CompassView) findViewById(R.id.compass);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+         mSensorManager.registerListener(compassView, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(compassView);
+    }
+}

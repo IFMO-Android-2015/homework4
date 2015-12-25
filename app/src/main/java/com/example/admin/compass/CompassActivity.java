46fa6c2bd5f72a7e9fdd86fc31b3e27a32572a53
CEMEHEX <CEMEHEX@yandex.ru>
2015-12-25 12:44:04 +0400

    'Done'

---
+++ app/src/main/java/com/example/admin/compass/CompassActivity.java
@@ -0,0 +1,41 @@
+package com.example.admin.compass;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class CompassActivity extends Activity implements SensorEventListener  {
+    private ImageView compassImage;
+    private float deg = 0F;
+    private SensorManager sensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        setContentView(R.layout.activity_compass);
+        compassImage = (ImageView) findViewById(R.id.CompassView);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float newDeg = Math.round(event.values[0]);
+        RotateAnimation rotateAnimation = new RotateAnimation(deg, -newDeg,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+        rotateAnimation.setDuration(500);
+        rotateAnimation.setFillAfter(true);
+        compassImage.startAnimation(rotateAnimation);
+        deg = -newDeg;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+}
